4c80e0a74aa0cda5cabc27db97ddf1fc73228732
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-20 18:26:21 +0000

    selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,38 +7,34 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = *list;
-	listint_t *sorted = NULL;
+	listint_t *head = NULL;
 	listint_t *temp;
-	listint_t *next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (current != NULL)
+	while (*list != NULL)
 	{
-		next = current->next;
-		if (sorted == NULL || current->n <= sorted->n)
+		*list = (*list)->next;
+		if (head == NULL || current->n < head->n)
 		{
-			current->next = sorted;
-			current->prev = NULL;
-			if (sorted != NULL)
-				sorted->prev = current;
-			sorted = current;
-			print_list(sorted);
+			current->next = head;
+			head = current;
 		}
 		else
 		{
-			temp = sorted;
-			while (temp->next != NULL && current->n > temp->next->n)
+			temp = head;
+			while (temp != NULL)
+			{
+				if (temp->next == NULL ||
+				    current->n < temp->next->n)
+				{
+					current->next = temp->next;
+					temp->next = current;
+					break;
+				}
 				temp = temp->next;
-			current->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = current;
-			temp->next = current;
-			current->prev = temp;
-			print_list(sorted);
+			}
 		}
-		current = next;
 	}
-	*list = sorted;
 }
